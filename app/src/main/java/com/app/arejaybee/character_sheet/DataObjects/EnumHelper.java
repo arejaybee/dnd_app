64727e863bc53f558cc0c715c0a68eb2dd28b1d9
arejaybee95 <arejaybee95@gmail.com>
2021-05-23 22:34:35 -0400

    Starting Over
    The java files made me sick to look back at. I am going to start over entirely with Kotlin and some better practices.

--- app/src/main/java/com/app/arejaybee/character_sheet/DataObjects/EnumHelper.java
+++
@@ -1,9 +0,0 @@
-package com.app.arejaybee.character_sheet.DataObjects;
-
-public class EnumHelper {
-    public enum PROFICIENCY {NONE, HALF, NORMAL, DOUBLE}
-
-    public enum EDITION {THREE_FIVE, FIFTH}
-
-    public enum MODIFIER {STR, DEX, CON, INT, WIS, CHA}
-}
