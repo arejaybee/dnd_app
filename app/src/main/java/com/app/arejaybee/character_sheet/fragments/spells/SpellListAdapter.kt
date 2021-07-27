b93c4a6ba11b585f7b9baba8f9d3481d6f807236
arejaybee95 <arejaybee95@gmail.com>
2021-07-26 22:20:14 -0400

    Spell Update
    Spells now have savable known/casts fields.

--- app/src/main/java/com/app/arejaybee/character_sheet/fragments/spells/SpellListAdapter.kt
+++ app/src/main/java/com/app/arejaybee/character_sheet/fragments/spells/SpellListAdapter.kt
@@ -4,11 +4,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import com.app.arejaybee.character_sheet.R
 import com.app.arejaybee.character_sheet.activity.MainActivity
 import com.app.arejaybee.character_sheet.data_objects.SpellList
+import com.app.arejaybee.character_sheet.utils.Util
 
 class SpellListAdapter(var dataSet: ArrayList<SpellList>, val activity: MainActivity, val parent: View) :
         RecyclerView.Adapter<SpellListAdapter.ViewHolder>() {
@@ -41,13 +42,25 @@ class SpellListAdapter(var dataSet: ArrayList<SpellList>, val activity: MainActi
 
         viewHolder.spellListBtn.setOnClickListener { l ->
             selectedButton?.isSelected = false
+            val casts = parent.findViewById<TextView>(R.id.spell_used)
+            val daily = parent.findViewById<TextView>(R.id.spell_daily)
+
             viewHolder.spellListBtn.isSelected = true
             selectedButton = viewHolder.spellListBtn
             val spellList = dataSet[position]
 
             parent.findViewById<RecyclerView>(R.id.spell_recycler).swapAdapter(SpellAdapter(spellList.spells, activity), true)
-            parent.findViewById<EditText>(R.id.adapter_spell_list_cast).setText(spellList.used.toString())
-            parent.findViewById<EditText>(R.id.adapter_spell_list_daily).setText(spellList.daily.toString())
+
+            Util.addNumberSpinnerToView(activity, activity.getString(R.string.spells_cast), casts, 0, {
+                activity.rob.spellLists[position].used = casts.text.toString().toInt()
+                activity.rob.saveCharacter()
+            })
+            Util.addNumberSpinnerToView(activity, activity.getString(R.string.spells_known), daily, 0, {
+                activity.rob.spellLists[position].daily = daily.text.toString().toInt()
+                activity.rob.saveCharacter()
+            })
+            casts.text = spellList.used.toString()
+            daily.text = spellList.daily.toString()
         }
     }
 
